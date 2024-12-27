f62e983109d4c17f6ec594f3261ab7b0b7ef9466
daniel <daniel.strielnikow@outlook.com>
2024-12-27 22:08:14 +0100

    update and delete

--- ecom-proj-back/src/main/java/pl/danielstrielnikow/ecomproj/controller/ProductController.java
+++ ecom-proj-back/src/main/java/pl/danielstrielnikow/ecomproj/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 import pl.danielstrielnikow.ecomproj.model.Product;
 import pl.danielstrielnikow.ecomproj.service.ProductService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -56,5 +57,33 @@ public class ProductController {
         return ResponseEntity.ok()
                 .contentType(MediaType.valueOf(product.getImageType()))
                 .body(imageFile);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id,
+                                                @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile) {
+        try {
+            Product product1 = service.updateProduct(id, product, imageFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Failed to Update", HttpStatus.BAD_REQUEST);
+        }
+
+        if (product != null) {
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("Failed to Update", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id) {
+        Product product = service.getProductById(id);
+        if (product != null) {
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+        }
     }
 }
